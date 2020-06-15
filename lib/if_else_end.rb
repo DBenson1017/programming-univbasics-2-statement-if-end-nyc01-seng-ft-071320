87503f9c0eb702fc1abdd639c15161ee5e4fdaa7
Devin Benson <devinbenson@gmail.com>
2020-06-15 22:29:53 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,12 +1,12 @@
 # Write your solution here
 
-time = Time.now.sec
-if time == even 
-  puts "Even!"
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time % 2 == 0  
+  puts "Even!" 
 else 
   puts "Odd!"
 end 
-
-
 
 
