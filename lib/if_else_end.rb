a68b7191ecf76909dbd5d49b8205071a8542a8ea
Devin Benson <devinbenson@gmail.com>
2020-06-15 22:10:00 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -7,3 +7,17 @@ puts "Code inside"
 end
 puts "code after if...end"
 
+chance_of_rain = 
+.2 
+if chance_of_rain
+  <= 0.25 
+  puts "Pack a sun shelter!"
+elsif 
+(chance_of_rain > 
+.25 && 
+chance_of_rain <
+0.75) 
+puts "Pack an umbrella!"
+else 
+  puts "Stay home and read Hegel."
+end
